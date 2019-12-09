63786216d0cad732a18dda91bd43acff7d3a15e2
Mykhailo Kazakov <mykhailo.kazakov@requestum.com>
2019-12-09 10:26:26 +0200

    added fetch GET request option

--- hw16/Nodejs/server.js
+++ hw16/Nodejs/server.js
@@ -1,12 +1,17 @@
-const http = require('http'); // подключаем http модуль!
+const http = require('http'); // подключаем http модуль
 http.createServer((req, res) => {
-  //res.end('HELLO WORLD');
-
-  // res.end(JSON.stringify({ name: 'SERGEi', lastName: 'SHAKHOV' })); //вывести в браузере
-  // console.log({ name: 'SERGEi', lastName: 'SHAKHOV' }); //вывести в консоль
-
-  // if (req.url === '/data') {
-  //   res.end('success');
-  // }
-  // res.end('failure');
+  res.setHeader("Access-Control-Allow-Origin", "*"); //for all requests
+  res.setHeader("Access-Control-Allow-Headers", "*"); //for POST request
+  if (req.url === '/post') {
+    console.log('it is POST'); //в консоль
+    let body = '';
+    req.on('data', (chunk) => {
+      body = String(chunk); //преобразуем в JSON
+    }).on('end', () => {
+      res.end(body); //выдаем пользователю JSON
+    })
+  } else {
+    console.log('it is GET') //в консоль
+    res.end('GET request'); //выдаем пользователю
+  }
 }).listen(3000, '127.0.0.1', () => console.log('Server is listening on port:' + 3000) );
